16fd990fef2fbc0a9d7f1c3830f4f974aa3efa85
Thirza-12 <thirzabehal@gmail.com>
2025-03-16 21:45:39 +0530

    added frontend url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cors from "cors";
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" })); 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://magenta-custard-b3d474.netlify.app/", credentials: true }));
 
 app.use(cookieParser());
 app.use("/api/auth", userRoutes);
